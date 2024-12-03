61cab49f09d24bfda19936c6852bfb6b1abb8e4d
Jesús Morán Espinoza <jame291104@gmail.com>
2024-12-02 21:40:14 -0500

    services and interfaces

---
+++ Frontend/src/app/interfaces/products.ts
@@ -0,0 +1,8 @@
+export interface Products {
+    image: string,
+    name: string,
+    category?: string,
+    price: number,
+    stock: number,
+    isAvailable?: false
+}
---
+++ Frontend/src/app/interfaces/users.ts
@@ -0,0 +1,6 @@
+export interface Users {
+    fullName: string,
+    email: string,
+    password: string,
+    role: "client",
+}
